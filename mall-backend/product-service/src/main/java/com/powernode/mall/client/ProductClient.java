16228604876a3e309135f86d29689330e6cea60d
WuhuFlyy <122222829+WuhuFlyy@users.noreply.github.com>
2024-08-26 16:43:04 +0800

    l

--- mall-backend/product-service/src/main/java/com/powernode/mall/client/ProductClient.java
+++ mall-backend/product-service/src/main/java/com/powernode/mall/client/ProductClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "shop-service", url = "http://10.107.240.158:80")
+@FeignClient(name = "shop-service", url = "http://10.110.248.238:80")
 public interface ProductClient {
     @RequestMapping("products/details")
     public JsonResult<ProductDetails> getProductById(@RequestParam("id") int id);
