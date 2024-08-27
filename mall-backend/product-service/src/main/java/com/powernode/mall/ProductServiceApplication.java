2c28258e8393096a4288bfd5acc823780f136045
WuhuFlyy <122222829+WuhuFlyy@users.noreply.github.com>
2024-08-27 12:34:25 +0800

    a

--- mall-backend/product-service/src/main/java/com/powernode/mall/ProductServiceApplication.java
+++ mall-backend/product-service/src/main/java/com/powernode/mall/ProductServiceApplication.java
@@ -3,10 +3,12 @@ package com.powernode.mall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrix
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
